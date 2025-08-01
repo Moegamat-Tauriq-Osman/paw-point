918fe5a7f2fa33dd2e9953ff88fff8422d612ce3
moega <moegamattauriqosman@gmail.com>
2025-08-01 18:41:35 +0200

    Paw Point

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin ',
+            'email' => 'admin@example.com',
+            'role' => 'Admin',
+            'password' => Hash::make('admin'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Staff ',
+            'email' => 'staff@example.com',
+            'role' => 'Staff Member',
+            'password' => Hash::make('staff'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'User ',
+            'email' => 'user@example.com',
+            'role' => 'User',
+            'password' => Hash::make('user'),
+        ]);
+
+        User::factory()->count(10)->create();
+    }
+}
