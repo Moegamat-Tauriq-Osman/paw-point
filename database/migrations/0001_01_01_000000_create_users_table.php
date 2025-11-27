7610551edca7fbc8ead3de3b48b85803dea9a409
moega <moegamattauriqosman@gmail.com>
2025-11-28 00:01:54 +0200

    .

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 255); 
+            $table->string('password');
             $table->enum('role', ['Admin', 'Staff', 'User'])->default('User');
 
             $table->rememberToken();
